6822345c4680bae767e69bbe96865bcedae7afac
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2025-03-11 23:51:20 -0400

    Auth Context Provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
       <h1 className='text-center pt-4 text-3xl'>React Supabase Auth & Context</h1>
-      <RouterProvider router={router}/>
+      <AuthContextProvider>
+        <RouterProvider router={router}/>
+      </AuthContextProvider>
     </>
   </StrictMode>,
 )
