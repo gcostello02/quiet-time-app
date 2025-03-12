82063766e1a48eb948e7942d4c199358ad195d9c
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2025-03-12 00:50:44 -0400

    Signup is Working

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext.jsx'
 
 const Signup = () => {
   const [email, setEmail] = React.useState('')
@@ -7,19 +8,49 @@ const Signup = () => {
   const [error, setError] = React.useState('')
   const [loading, setLoading] = React.useState('')
 
+  const {signUpNewUser } = UserAuth()
+  const navigate = useNavigate()
+
+  const handleSignUp = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+    try {
+      const result = await signUpNewUser(email, password)
+
+      if (result.success) {
+        navigate('/dashboard')
+      }
+    } catch (error) {
+      setError("An error occured", error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <div>
-      <form className='max-w-md m-auto pt-24'>
+      <form onSubmit={handleSignUp} className='max-w-md m-auto pt-24'>
         <h2 className='font-bold pb-2'>Sign up today!</h2>
         <p>
           Already have an account? <Link to="/signin">Sign in!</Link>
         </p>
         <div className='flex flex-col py-4'>
-          <input placeholder="Email" className='p-3 mt-6' type='email' />
-          <input placeholder='Password' className='p-3 mt-6' type="password" />
+          <input 
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email" 
+            className='p-3 mt-6' 
+            type='email' 
+          />
+          <input 
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='Password' 
+            className='p-3 mt-6' 
+            type="password" 
+          />
           <button type='submit' className='mt-6 w-full'>
             Sign up
           </button>
+          {error && <p className='text-red-600 text-center pt-4'>{error}</p>}
         </div>
       </form>
     </div>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,12 +1,59 @@
 import { createContext, useEffect, useState, useContext } from "react";
+import { supabase } from "../supabaseClient";
 
 const AuthContext = createContext()
 
 export const AuthContextProvider = ({ children }) => {
   const [session, setSession] = useState(undefined)
 
+  const signUpNewUser = async (email, password) => {
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    })
+
+    if (error) {
+      console.log("Error signing up user:", error)
+      return { success: false, error }
+    }
+    return { success: true, data }
+  }
+
+  const signInUser =  async (email, password) => {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      })
+      if (error) {
+        console.error("Error signing in user:", error)
+        return { success: false, error: error.message }
+      }
+      return { success: true, data }
+    } catch (error) {
+      console.error("Error signing in user:", error)
+    }
+  }
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  const signOut = () => {
+    const { error } = supabase.auth.signOut()
+    if (error) {
+      console.error("Error signing out:", error)
+    } 
+  }
+
   return (
-    <AuthContext.Provider value={{session}}>
+    <AuthContext.Provider value={{ session, signUpNewUser, signInUser, signOut }}>
       {children}
     </AuthContext.Provider>
   )
