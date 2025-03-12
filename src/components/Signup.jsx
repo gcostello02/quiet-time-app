12cfa2d5e147acaa7837ffec52f5828962b281a3
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2025-03-11 23:49:18 -0400

    Beginning of Signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [error, setError] = React.useState('')
+  const [loading, setLoading] = React.useState('')
+
   return (
-    <div>Signup</div>
+    <div>
+      <form className='max-w-md m-auto pt-24'>
+        <h2 className='font-bold pb-2'>Sign up today!</h2>
+        <p>
+          Already have an account? <Link to="/signin">Sign in!</Link>
+        </p>
+        <div className='flex flex-col py-4'>
+          <input placeholder="Email" className='p-3 mt-6' type='email' />
+          <input placeholder='Password' className='p-3 mt-6' type="password" />
+          <button type='submit' className='mt-6 w-full'>
+            Sign up
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
