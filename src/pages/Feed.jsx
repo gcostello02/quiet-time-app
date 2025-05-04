579b30fcc4cee9eb1a120147fe6d53dc89c9ec11
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2025-05-03 21:35:22 -0400

    Some functionality on feed page

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,11 +1,52 @@
 import Navbar from "../components/Navbar";
+import { UserAuth } from "../context/AuthContext";
+import { useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
 
 const Feed = () => {
+  const { session } = UserAuth(); 
+  const [loading, setLoading] = useState(true); 
 
+  useEffect(() => {
+    if(session?.user?.id) {
+      fetchPosts()
+    }
+  }, [session?.user?.id])
+
+  const fetchPosts = async () => {
+    setLoading(true)
+
+    const { data: posts, error: error } = await supabase
+      .from('notes')
+      .select('*')
+
+    if (error) {
+      console.error("Error fetching posts:", error);
+      setLoading(false);
+      return;
+    }
+
+    console.log(posts)
+    setLoading(false)
+  }
+
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <Navbar />
+        <div className="flex justify-center items-center mt-5">
+          <div className="spinner-border animate-spin h-8 w-8 border-4 border-t-transparent border-indigo-600 rounded-full"></div>
+        </div>
+      </div>
+    );
+  }
+  
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900">      
       <Navbar />
-      
+      <div className="max-w-3xl mx-auto p-6 space-y-8">
+        
+      </div>
     </div>
   );
 };
