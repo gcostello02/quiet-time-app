67e7a6c6de13647e8a163a0b8b45801635a036ed
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2025-05-14 08:05:17 -0400

    Private Route Spinner

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -6,7 +6,15 @@ const PrivateRoute = ({ children }) => {
   const { session } = UserAuth();
 
   if (session === undefined) {
-    return <div>Loading...</div>;
+    return (
+      <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
+        <main className="flex-grow">
+          <div className="flex justify-center items-center mt-10">
+            <div className="spinner-border animate-spin h-8 w-8 border-4 border-t-transparent border-indigo-600 rounded-full"></div>
+          </div>
+        </main>
+      </div>
+    );
   }
 
   return <div>{session ? <>{children}</> : <Navigate to="/signin" />}</div>;
